9f49dfb51cf4990b873881fd163b7366ad8a14a9
Nasir Noor <nasir@nasir.id>
2025-07-10 10:43:54 +0700

    update vite config

--- app/frontend/vite.config.js
+++ app/frontend/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // This allows Vite to receive requests from the specified host.
+    // It's a security measure to prevent DNS rebinding attacks.
+    allowedHosts: ['gke.react-movie.nasir.id'],
+  },
 })
